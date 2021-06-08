6555c3764835fb5cc1dd4c8ff88ecf4847d0ec08
MiN1One <git_min1one@mail.ru>
2021-06-08 14:16:30 +0500

    Temporarily disable telegram bot

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,8 @@
-const bot = require('../telegram');
+// const bot = require('../telegram');
 
 exports.acceptOrder = (req, res) => {
-    bot.getChatMember(827905012, 827905012)
-        .then(d => console.log(d));
+    // bot.getChatMember(827905012, 827905012)
+    //     .then(d => console.log(d));
 
 
     // name: fnameRef.current.value,
@@ -24,44 +24,44 @@ exports.acceptOrder = (req, res) => {
     //     "type":"skins"
     // }
 
-    const form = { ...req.body };
+    // const form = { ...req.body };
 
-    const { name } = req.body;
-    const fields = [];
-    // const orders = [];
+    // const { name } = req.body;
+    // const fields = [];
+    // // const orders = [];
 
-    delete form.name;
-    delete form.order;
-    !form.email && delete form.email;
-    !form.address && delete form.address;
+    // delete form.name;
+    // delete form.order;
+    // !form.email && delete form.email;
+    // !form.address && delete form.address;
 
-    for (let [key, val] of Object.entries(form)) {
-        if (typeof val !== Object) fields.push(`${key}: ${val}`);
-    }
+    // for (let [key, val] of Object.entries(form)) {
+    //     if (typeof val !== Object) fields.push(`${key}: ${val}`);
+    // }
 
-    const orders = req.body.order.items.map((el) => {
-        const itemFields = [];
-        for (let [key, val] of Object.entries(el)) {
-            itemFields.push(`${key}: ${val}`);
-        }
-        console.log(itemFields.join('\n'));
-        return itemFields.join('\n');
-    }).join('\n-----------------------\n');
+    // const orders = req.body.order.items.map((el) => {
+    //     const itemFields = [];
+    //     for (let [key, val] of Object.entries(el)) {
+    //         itemFields.push(`${key}: ${val}`);
+    //     }
+    //     console.log(itemFields.join('\n'));
+    //     return itemFields.join('\n');
+    // }).join('\n-----------------------\n');
 
-    console.log(orders);
+    // console.log(orders);
 
-    fields.unshift(`---------- <u>${name}</u> ----------`);
+    // fields.unshift(`---------- <u>${name}</u> ----------`);
 
-    bot.sendMessage(
-        827905012,
-        [fields.join('\n'), orders].join('\n'), 
-        { parse_mode: 'HTML' }
-    );
+    // bot.sendMessage(
+    //     827905012,
+    //     [fields.join('\n'), orders].join('\n'), 
+    //     { parse_mode: 'HTML' }
+    // );
 
-    console.log(fields.toString().split(',').join('\n'));
+    // console.log(fields.toString().split(',').join('\n'));
 
     res.status(201).json({
         status: 'success',
-        data: req.body
+        data: {}
     });
 };
